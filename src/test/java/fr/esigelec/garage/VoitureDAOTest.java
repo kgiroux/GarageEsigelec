ad3eb70a74cf02316c681183cbf7601dad5f2e86
Kévin Giroux <giroux.kevin.fr@gmail.com>
2015-11-13 14:06:19 +0100

    Commit Dao Java

--- src/test/java/fr/esigelec/garage/VoitureDAOTest.java
+++ src/test/java/fr/esigelec/garage/VoitureDAOTest.java
@@ -16,8 +16,25 @@ public class VoitureDAOTest {
 	}
 	
 	@Test
-	public void getVoitureTest(){
+	public void getVoitureTestIndexZero(){
 		VoitureDAO dao = new VoitureDAO();
+		System.out.println(dao.get(0));
 		assertNull(dao.get(0));
 	}
+	
+	
+	@Test
+	public void getVoitureTestIndexNegatif(){
+		VoitureDAO dao = new VoitureDAO();
+		assertNull(dao.get(-1));
+	}
+	
+	@Test
+	public void getVoitureTestAfterAdd(){
+		VoitureDAO dao = new VoitureDAO();
+		Voiture v = new Voiture(0,5000,"ABC-DEF-GHI","CITROEN",2009);
+		dao.ajouter(v);
+		assertEquals(v, dao.get(v.getId()));
+	}
+	
 }
