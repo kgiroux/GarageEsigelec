5302fd83fb3f3c1822932d744954841c76a3d80b
LirycMoaners <cyril.lefebvre.27@gmail.com>
2015-11-13 15:03:18 +0100

    getListTest

--- src/test/java/fr/esigelec/garage/VoitureDAOTest.java
+++ src/test/java/fr/esigelec/garage/VoitureDAOTest.java
@@ -34,7 +34,22 @@ public class VoitureDAOTest {
 		dao.supprimer(v);
 	}
 	
+	public void getVoitureListVideTest(){
+		VoitureDAO dao = new VoitureDAO();
+		assertNull(dao.getVoitures());
+	}
 	
+	public void getVoitureListAfterAddTest(){
+		VoitureDAO dao = new VoitureDAO();
+		Voiture v = new Voiture(0,5000,"ABC-DEF-GHI","CITROEN",2009);
+		Voiture w = new Voiture(1,1,"JKL-MNO-PQR","PEUGEOT",-500);
+		dao.ajouter(v);
+		dao.ajouter(w);
+		assertEquals(dao.getVoitures().size(), 2);
+		dao.supprimer(v);
+		dao.supprimer(w);
+		dao.supprimer(v);
+	}
 	
 	
 	
